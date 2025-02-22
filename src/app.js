042a290314fcc0da02495f5779c96f8ea9d019df
widenhrzns <widenhrzns@gmail.com>
2025-02-22 20:33:36 +0300

    State и реактивности

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
-import { MainView } from "./views/main/main";
+import { MainView } from "./views/main/main.js";
 
 class App {
   routes = [{ path: "", view: MainView }];
+  appState = {
+    favorites: [],
+  };
 
   constructor() {
     window.addEventListener("hashchange", this.route.bind(this));
@@ -13,7 +16,7 @@ class App {
       this.currentView.destroy();
     }
     const view = this.routes.find((route) => route.path == location.hash).view;
-    this.currentView = new view();
+    this.currentView = new view(this.appState);
     this.currentView.render();
   }
 }
